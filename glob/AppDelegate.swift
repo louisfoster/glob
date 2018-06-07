83bb19473c54cb5c58ae7e994270f550a60ed8b2
Louis Foster <louis@airteam.com.au>
2018-06-07 21:11:19 +1000

    Initial Commit

---
+++ glob/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  glob
+//
+//  Created by Louis Foster on 7/6/18.
+//  Copyright © 2018 Louis Foster. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
